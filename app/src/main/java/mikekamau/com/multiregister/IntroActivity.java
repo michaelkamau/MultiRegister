e920169945f368d130f9b87a8ce16901e625a807
Michael Kamau <mikekamau@Michaels-MacBook-Pro.local>
2018-03-16 11:40:25 +0300

    Initial Projecty setup

---
+++ app/src/main/java/mikekamau/com/multiregister/IntroActivity.java
@@ -0,0 +1,17 @@
+package mikekamau.com.multiregister;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Intro screen before user starts registration process
+ */
+public class IntroActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
+
