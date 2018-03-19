2f1e7eab2488e72c5bda669cd5fbb29b0f1f42a6
Michael Kamau <tokamau@gmail.com>
2018-03-19 14:19:26 +0300

    Add layout create username

--- app/src/main/java/mikekamau/com/multiregister/IntroActivity.java
+++ app/src/main/java/mikekamau/com/multiregister/IntroActivity.java
@@ -2,16 +2,24 @@ package mikekamau.com.multiregister;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 /**
  * Intro screen before user starts registration process
  */
 public class IntroActivity extends AppCompatActivity{
 
+    private Button btnStartRegistration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        btnStartRegistration = findViewById(R.id.btn_register);
+
     }
+
+
 }
 
