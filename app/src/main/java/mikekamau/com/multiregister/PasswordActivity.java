5526b279a9dc6f0121f13bd679fc0bbdc5381924
Michael Kamau <tokamau@gmail.com>
2018-03-20 17:44:25 +0300

    Add activities names

---
+++ app/src/main/java/mikekamau/com/multiregister/PasswordActivity.java
@@ -0,0 +1,17 @@
+package mikekamau.com.multiregister;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mikekamau on 3/20/18.
+ */
+
+public class PasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_password);
+    }
+}
